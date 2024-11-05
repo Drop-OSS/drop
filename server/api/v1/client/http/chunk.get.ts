b9ae26cb27b35e6d24b6729bed31439a92ddba19
DecDuck <declanahofmeyr@gmail.com>
2024-11-06 09:55:38 +1100

    chore: remove client API deadweight

--- server/api/v1/client/http/chunk.get.ts
+++
@@ -1,9 +0,0 @@
-import { defineClientEventHandler } from "~/server/internal/clients/event-handler";
-
-export default defineClientEventHandler(async (h3) => {
-  const query = getQuery(h3);
-
-  const gameId = query.game;
-  const versionName = query.version;
-  const chunkId = query.chunk;
-});
--- server/api/v1/client/metadata/endpoints.get.ts
+++
@@ -1,5 +0,0 @@
-import { defineClientEventHandler } from "~/server/internal/clients/event-handler";
-
-export default defineClientEventHandler(async (h3) => {
-
-});
