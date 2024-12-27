4f9b94921a730912b2e8419a3736c094d9f3be84
DecDuck <declanahofmeyr@gmail.com>
2024-12-27 12:43:42 +1100

    feat(ui): add noWrapper option

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="flex flex-col w-full min-h-screen bg-zinc-900">
+  <div v-if="!noWrapper" class="flex flex-col w-full min-h-screen bg-zinc-900">
     <UserHeader class="z-50" />
     <div class="grow flex">
       <NuxtPage />
     </div>
     <UserFooter class="z-50" />
   </div>
+  <div class="flex w-full min-h-screen bg-zinc-900" v-else>
+    <NuxtPage />
+  </div>
 </template>
 
 <script setup lang="ts">
+const route = useRoute();
+const noWrapper = !!route.query.noWrapper;
+
 useHead({
   titleTemplate(title) {
     if (title) return `${title} | Drop`;
