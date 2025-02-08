97792f0707dcdfdad351a82ad444af1da47b570a
DecDuck <declanahofmeyr@gmail.com>
2025-02-08 11:41:16 +1100

    fix: home page now (temporarily) redirects to store

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,5 @@
 <template>
-    <div class="px-12 py-4">
-        <h1 class="text-zinc-100 text-2xl font-bold font-display">
-            Newly added
-        </h1>
-        <NuxtLink class="text-blue-600 font-semibold"
-            >Explore more &rarr;</NuxtLink
-        >
-        <div class="mt-4 grid grid-cols-8 gap-x-8 w-max">
-            <GamePanel v-for="i in 8" :game="games[i - 1]" />
-        </div>
-    </div>
+    
 </template>
 
 <script setup lang="ts">
@@ -17,6 +7,6 @@ useHead({
     title: "Home",
 });
 
-const headers = useRequestHeaders(["cookie"]);
-const games = await $fetch("/api/v1/store/recent", { headers });
+const router = useRouter();
+router.replace('/store')
 </script>
