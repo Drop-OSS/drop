9d07070ef60d8505074665b3a13b7bfb6ebbbfff
DecDuck <declanahofmeyr@gmail.com>
2025-03-11 18:24:50 +1100

    feat: add news client routes

---
+++ server/api/v1/client/news/index.get.ts
@@ -0,0 +1,29 @@
+import { defineClientEventHandler } from "~/server/internal/clients/event-handler";
+import newsManager from "~/server/internal/news";
+
+export default defineClientEventHandler(async (h3) => {
+  const query = getQuery(h3);
+
+  const orderBy = query.order as "asc" | "desc";
+  if (orderBy) {
+    if (typeof orderBy !== "string" || !["asc", "desc"].includes(orderBy))
+      throw createError({ statusCode: 400, statusMessage: "Invalid order" });
+  }
+
+  const tags = query.tags as string[] | undefined;
+  if (tags) {
+    if (typeof tags !== "object" || !Array.isArray(tags))
+      throw createError({ statusCode: 400, statusMessage: "Invalid tags" });
+  }
+
+  const options = {
+    take: parseInt(query.limit as string),
+    skip: parseInt(query.skip as string),
+    orderBy: orderBy,
+    tags: tags?.map((e) => e.toString()),
+    search: query.search as string,
+  };
+
+  const news = await newsManager.fetch(options);
+  return news;
+});
