0edfdbdfce47291a200782a470c0d1db1edfc9ae
DecDuck <declanahofmeyr@gmail.com>
2025-04-05 15:34:24 +1100

    fix: return user library for client

--- server/api/v1/client/user/library.get.ts
+++ server/api/v1/client/user/library.get.ts
@@ -1,8 +1,9 @@
 import { defineClientEventHandler } from "~/server/internal/clients/event-handler";
 import prisma from "~/server/internal/db/database";
+import userLibraryManager from "~/server/internal/userlibrary";
 
-export default defineClientEventHandler(async (h3) => {
-    // TODO return the user's library
-    const games = await prisma.game.findMany({});
-    return games;
+export default defineClientEventHandler(async (h3, { fetchUser }) => {
+  const user = await fetchUser();
+  const library = await userLibraryManager.fetchLibrary(user.id);
+  return library.entries.map((e) => e.game);
 });
