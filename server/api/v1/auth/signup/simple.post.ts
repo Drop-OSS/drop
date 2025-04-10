ca03be7f43b41c52c9195fab8c23735c2ed35b4d
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2025-04-10 19:50:04 -0400

    fix: don't add write perms, users can only delete objects

--- server/api/v1/auth/signup/simple.post.ts
+++ server/api/v1/auth/signup/simple.post.ts
@@ -64,7 +64,7 @@ export default defineEventHandler(async (h3) => {
     profilePictureId,
     async () => jdenticon.toPng(user.username, 256),
     {},
-    [`internal:read`, `${userId}:write`]
+    [`internal:read`, `${userId}:delete`]
   );
   const [linkMec] = await prisma.$transaction([
     prisma.linkedAuthMec.create({
