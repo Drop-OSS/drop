c0c55d35f4e4882376d248c2ad5725299077335d
DecDuck <declanahofmeyr@gmail.com>
2025-05-07 10:43:27 +1000

    fix: signout route

--- server/routes/auth/signout.get.ts
+++ server/routes/auth/signout.get.ts
@@ -1,4 +1,4 @@
-import sessionHandler from "../internal/session";
+import sessionHandler from "../../internal/session";
 
 export default defineEventHandler(async (h3) => {
   await sessionHandler.signout(h3);
