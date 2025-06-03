f1286c0e86c6c2e1d12058e8a4292bf03ff464ce
Aden <aden@adenmgb.com>
2025-06-03 12:48:01 +0930

    chore: fix preetier eslint issue

--- pages/account/notifications.vue
+++ pages/account/notifications.vue
@@ -54,7 +54,13 @@
             </div>
             <div class="ml-4 flex flex-shrink-0 items-center gap-x-2">
               <span class="text-xs text-zinc-500">
-                {{ DateTime.fromISO(typeof notification.created === 'string' ? notification.created : notification.created.toISOString()).toRelative() }}
+                {{
+                  DateTime.fromISO(
+                    typeof notification.created === "string"
+                      ? notification.created
+                      : notification.created.toISOString(),
+                  ).toRelative()
+                }}
               </span>
               <button
                 v-if="!notification.read"
