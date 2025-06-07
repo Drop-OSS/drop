6453e03d0336130174bebfdb163f40905735dd32
DecDuck <declanahofmeyr@gmail.com>
2025-06-07 19:11:05 +1000

    fix: translations in title bar not updating

--- components/UserHeader.vue
+++ components/UserHeader.vue
@@ -206,7 +206,7 @@ const router = useRouter();
 const { t } = useI18n();
 
 const homepageURL = "/store";
-const navigation: Array<NavigationItem> = [
+const navigation: Ref<Array<NavigationItem>> = computed(() => [
   {
     prefix: "/store",
     route: "/store",
@@ -227,9 +227,9 @@ const navigation: Array<NavigationItem> = [
     route: "/news",
     label: t("userHeader.links.news"),
   },
-];
+]);
 
-const currentPageIndex = useCurrentNavigationIndex(navigation);
+const currentPageIndex = useCurrentNavigationIndex(navigation.value);
 
 const notifications = useNotifications();
 const unreadNotifications = computed(() =>
