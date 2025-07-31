b7880003da4d642471020b3f9c81b08bc149be2b
DecDuck <declanahofmeyr@gmail.com>
2025-07-31 13:02:35 +1000

    fix: require session on code fetch endpoint

--- server/api/v1/client/auth/code/index.get.ts
+++ server/api/v1/client/auth/code/index.get.ts
@@ -1,6 +1,10 @@
 import clientHandler from "~/server/internal/clients/handler";
+import sessionHandler from "~/server/internal/session";
 
 export default defineEventHandler(async (h3) => {
+  const user = await sessionHandler.getSession(h3);
+  if (!user) throw createError({ statusCode: 403 });
+
   const query = getQuery(h3);
   const code = query.code?.toString()?.toUpperCase();
   if (!code)
