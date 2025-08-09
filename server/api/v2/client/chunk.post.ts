28f9269b92eecda2e61af3d671aedd1ba6fa1903
DecDuck <declanahofmeyr@gmail.com>
2025-08-09 11:12:35 +1000

    feat: v2 download API

---
+++ server/api/v2/client/chunk.post.ts
@@ -0,0 +1,73 @@
+import { type } from "arktype";
+import { readDropValidatedBody, throwingArktype } from "~/server/arktype";
+import contextManager from "~/server/internal/downloads/coordinator";
+import libraryManager from "~/server/internal/library";
+
+const GetChunk = type({
+  context: "string",
+  files: type({
+    filename: "string",
+    chunkIndex: "number",
+  }).array(),
+}).configure(throwingArktype);
+
+export default defineEventHandler(async (h3) => {
+  const body = await readDropValidatedBody(h3, GetChunk);
+
+  const context = await contextManager.fetchContext(body.context);
+  if (!context)
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Invalid download context.",
+    });
+
+  const streamFiles = [];
+
+  for (const file of body.files) {
+    const manifestFile = context.manifest[file.filename];
+    if (!manifestFile)
+      throw createError({
+        statusCode: 400,
+        statusMessage: `Unknown file: ${file.filename}`,
+      });
+
+    const start = manifestFile.lengths
+      .slice(0, file.chunkIndex)
+      .reduce((a, b) => a + b, 0);
+    const end = start + manifestFile.lengths[file.chunkIndex];
+
+    streamFiles.push({ filename: file.filename, start, end });
+  }
+
+  setHeader(
+    h3,
+    "Content-Lengths",
+    streamFiles.map((e) => e.end - e.start).join(","),
+  ); // Non-standard header, but we're cool like that 😎
+
+  for (const file of streamFiles) {
+    const gameReadStream = await libraryManager.readFile(
+      context.libraryId,
+      context.libraryPath,
+      context.versionName,
+      file.filename,
+      { start: file.start, end: file.end },
+    );
+    if (!gameReadStream)
+      throw createError({
+        statusCode: 500,
+        statusMessage: "Failed to create read stream",
+      });
+    await gameReadStream.pipeTo(
+      new WritableStream({
+        write(chunk) {
+          h3.node.res.write(chunk);
+        },
+      }),
+    );
+  }
+
+  await h3.node.res.end();
+
+  return;
+});
--- server/internal/downloads/coordinator.ts
+++ server/internal/downloads/coordinator.ts
@@ -1,9 +1,68 @@
-/*
-The download co-ordinator's job is to keep track of all the currently online clients.
+import prisma from "../db/database";
+import type { DropManifest } from "./manifest";
 
-When a client signs on and registers itself as a peer
+const TIMEOUT = 1000 * 60 * 60 * 1; // 1 hour
 
-*/
+class DownloadContextManager {
+  private contexts: Map<
+    string,
+    {
+      timeout: Date;
+      manifest: DropManifest;
+      versionName: string,
+      libraryId: string;
+      libraryPath: string;
+    }
+  > = new Map();
 
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class, @typescript-eslint/no-unused-vars
-class DownloadCoordinator {}
+  async createContext(game: string, versionName: string) {
+    const version = await prisma.gameVersion.findUnique({
+      where: {
+        gameId_versionName: {
+          gameId: game,
+          versionName,
+        },
+      },
+      include: {
+        game: {
+          select: {
+            libraryId: true,
+            libraryPath: true,
+          },
+        },
+      },
+    });
+    if (!version) return undefined;
+
+    const contextId = crypto.randomUUID();
+    this.contexts.set(contextId, {
+      timeout: new Date(),
+      manifest: JSON.parse(version.dropletManifest as string) as DropManifest,
+      versionName,
+      libraryId: version.game.libraryId!,
+      libraryPath: version.game.libraryPath,
+    });
+
+    return contextId;
+  }
+
+  async fetchContext(contextId: string) {
+    const context = this.contexts.get(contextId);
+    if (!context) return undefined;
+    context.timeout = new Date();
+    this.contexts.set(contextId, context);
+    return context;
+  }
+
+  async cleanup() {
+    for (const key of this.contexts.keys()) {
+      const context = this.contexts.get(key)!;
+      if (context.timeout.getDate() + TIMEOUT < Date.now()) {
+        this.contexts.delete(key);
+      }
+    }
+  }
+}
+
+export const contextManager = new DownloadContextManager();
+export default contextManager;
