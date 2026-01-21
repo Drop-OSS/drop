5112eb28d0403b557fcbad0df52e789e5713ebd3
Francois Ribemont <ribemont.francois@gmail.com>
2026-01-21 08:05:48 +0000

    Adds label customization through an env variable for the oidc sign in button #325

--- components/Auth/OpenID.vue
+++ components/Auth/OpenID.vue
@@ -4,7 +4,14 @@
       :href="`/auth/oidc?redirect=${route.query.redirect ?? '/'}`"
       class="transition rounded-md grow inline-flex items-center justify-center bg-white/10 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-white/20"
     >
-      <i18n-t keypath="auth.signin.externalProvider" tag="span" scope="global">
+      <i18n-t
+        keypath="auth.signin.signinWithExternalProvider"
+        tag="span"
+        scope="global"
+      >
+        <template #externalProvider>{{
+          providerName || $t("auth.signin.externalProvider")
+        }}</template>
         <template #arrow>
           <span aria-hidden="true">{{ $t("chars.arrow") }}</span>
         </template>
@@ -15,4 +22,6 @@
 
 <script setup lang="ts">
 const route = useRoute();
+
+const { providerName = undefined } = defineProps<{ providerName?: string }>();
 </script>
