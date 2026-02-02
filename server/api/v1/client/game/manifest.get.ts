d093316357e742b176e7edc4e586721783324a2a
John Doe <johndoe@example.com>
2026-02-02 10:10:12 -0500

    authentiction fix

--- server/api/v1/client/game/manifest.get.ts
+++ server/api/v1/client/game/manifest.get.ts
@@ -1,7 +1,6 @@
-import { defineClientEventHandler } from "~/server/internal/clients/event-handler";
 import { createDownloadManifestDetails } from "~/server/internal/library/manifest/index";
 
-export default defineClientEventHandler(async (h3) => {
+export default defineEventHandler(async (h3) => {
   const query = getQuery(h3);
   const version = query.version?.toString();
   if (!version)
