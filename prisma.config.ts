611ec69480869f355fd843b0a6c1874005813b35
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2026-02-08 11:15:49 -0500

    fix prisma cli breaking things

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,17 @@
 import { config } from "dotenv";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 import path from "node:path";
 
 // load .env variables
 config();
 
+// need to use built in env access as prisma's env function throws when DATABASE_URL is undefined
+// this is acceptable as prisma throws already it actually needs the URL
+const databaseURL = process.env.DATABASE_URL;
+
 export default defineConfig({
   schema: path.join("prisma"),
   datasource: {
-    url: env("DATABASE_URL"),
+    url: databaseURL,
   },
 });
