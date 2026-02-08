7186c9bc1924b675a569d678cc52d7e2154293d9
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2026-02-08 18:26:33 -0500

    fix prisma breaking if extension already exists

--- prisma/migrations/20250721062509_add_pg_trgm/migration.sql
+++ prisma/migrations/20250721062509_add_pg_trgm/migration.sql
@@ -1,5 +1,5 @@
 -- Add pg_trgm
-CREATE EXTENSION pg_trgm;
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
 -- Create index for tag names
 -- CREATE INDEX trgm_tag_name ON "Tag" USING GIST (name gist_trgm_ops(siglen=32));
